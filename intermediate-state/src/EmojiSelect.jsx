fd07791528ea72f7b10ea7b50dc5f982464e36da
Pratik Kharadhara <pratikkharadhara@gmail.com>
2026-03-04 14:15:29 +0530

    uuid for key in array

--- intermediate-state/src/EmojiSelect.jsx
+++ intermediate-state/src/EmojiSelect.jsx
@@ -1,18 +1,42 @@
+// import { useState } from "react"
+// // here we are updating the array with use State
+// export default function EmojiSelect(){
+//     const[emoji,setEmoji]=useState(["😁"]);//declaring the array with one emoji
+//     function addEmoji(){
+//            setEmoji([...emoji,"😔"]);//same as obejcts use spread operator to create a new copy of the same 
+//            //array and then set the emoji to the new copy array
+//     }
+//     return (
+//         <>
+//             <h2>updating array using use state</h2>
+//             {
+//                 emoji.map((e)=>{ //using map we are iterating all over the array to print the emojis present in the array
+//                     return  <span style={{fontSize:"4rem"}}>{e}</span>
+//                  //just returning the span of the array of the emojis
+//                 })
+//             }
+//             <br />
+//             <button onClick={addEmoji}>click to add</button>
+//         </>
+//     )
+// }
+
+//now we need to setup the uuid for each component in the array
+//so that we can delete them in future
 import { useState } from "react"
-// here we are updating the array with use State
+import {v4 as uuid} from "uuid";
 export default function EmojiSelect(){
-    const[emoji,setEmoji]=useState(["😁"]);//declaring the array with one emoji
+    const[emoji,setEmoji]=useState([{id:uuid(),emoji:"😁"}]);//declaring the array with one emoji and uuid for id 
     function addEmoji(){
-           setEmoji([...emoji,"😔"]);//same as obejcts use spread operator to create a new copy of the same 
-           //array and then set the emoji to the new copy array
+           setEmoji([...emoji,{id:uuid(),emoji:"😴"}]);//
     }
     return (
         <>
             <h2>updating array using use state</h2>
             {
                 emoji.map((e)=>{ //using map we are iterating all over the array to print the emojis present in the array
-                    return  <span style={{fontSize:"4rem"}}>{e}</span>
-                 //just returning the span of the array of the emojis
+                    return  <span key={e.id} style={{fontSize:"4rem"}}>{e.emoji}</span>//if we dont write e.emoji cant access the emoji icon
+                 //using the uuid we are generating random ids for our items 
                 })
             }
             <br />
